b354ba69e7d2d84621d0a6fe9fea64eda8bebe89
Calum Murray <Cal_92@hotmail.co.uk>
2013-02-27 19:11:27 +0000

    Clean project

---
+++ src/com/hacku/swearjar/MainLayoutActivity.java
@@ -0,0 +1,24 @@
+package com.hacku.swearjar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainLayoutActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_layout, menu);
+        return true;
+    }
+
+    
+}
