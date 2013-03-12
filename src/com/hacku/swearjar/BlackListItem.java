545ff64eb260ae91ad345f7f4c08174cedf2ba0b
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-03-12 20:27:29 +0000

    Changed charge from float to BigDecimal and made a method to get it as a string formatted for currency.

--- src/com/hacku/swearjar/BlackListItem.java
+++ src/com/hacku/swearjar/BlackListItem.java
@@ -1,22 +1,25 @@
 package com.hacku.swearjar;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 public class BlackListItem implements Serializable {
 
 	private String word;
-	private float charge;
+	private BigDecimal charge;
 	private int occurrences;
+
 	public String getWord() {
 		return word;
 	}
 	public void setWord(String word) {
 		this.word = word;
 	}
-	public float getCharge() {
+	public BigDecimal getCharge() {
 		return charge;
 	}
-	public void setCharge(float charge) {
+	public void setCharge(BigDecimal charge) {
 		this.charge = charge;
 	}
 	public int getOccurrences() {
@@ -24,5 +27,12 @@ public class BlackListItem implements Serializable {
 	}
 	public void setOccurrences(int occurrences) {
 		this.occurrences = occurrences;
+	}
+	/**
+	 * @return charge as a String formatted as local currency
+	 */
+	public String formatCharge(){
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		return nf.format(charge);
 	}
 }
