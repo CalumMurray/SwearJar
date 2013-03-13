0f7eaf8a612a0946cc4363872f7aadab1f883039
Calum Murray <Cal_92@hotmail.co.uk>
2013-03-13 20:50:21 +0000

    addOccurrences responsibilty moved to SwearJarApplication for consistency

--- src/com/hacku/swearjar/BlackListItem.java
+++ src/com/hacku/swearjar/BlackListItem.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-import org.apache.commons.lang3.StringUtils;
 
 //Entity bean class to represent a single blacklisted word triple (word, charge, occurrences)
 public class BlackListItem implements Serializable {
@@ -82,17 +81,5 @@ public class BlackListItem implements Serializable {
 	public static boolean validateCurrency(String string) {
 		return string.matches("^[ ]*[\\d]+[.][\\d][\\d]$");
 	}
-	
-	/**
-	 * Adds to occurrences the number of times that the word it appears 
-	 * in the utterance.  Not case sensitive.
-	 * 
-	 * @param utterance to search for word
-	 * @return number of times this BlackListItem's word appears in utterance
-	 */
-	public int addOccurrences(String utterance){
-		int occurrences = StringUtils.countMatches(utterance.toUpperCase(), word.toUpperCase());  //Find the number of matches
-		this.occurrences += occurrences; //Add the matches to the total matches
-		return occurrences;
-	}
+
 }
